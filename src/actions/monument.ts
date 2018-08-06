0df64873bf05d95f8d7b9c44a6fbdb8a6302cfa3
Bronwyn Harris <bronwyn.harris@Bronwyns-MacBook-Air.local>
2018-08-05 21:16:24 -0400

    more images work

--- src/actions/monument.ts
+++ src/actions/monument.ts
@@ -1,6 +1,6 @@
 import { SET_MONUMENTS, SET_PHOTOS } from '../constants/monument';
 
-const api = (monuments = 'monuments') => `https://api.myjson.com/bins/14mrqw`;
+const api = (monuments = 'monuments') => `https://api.myjson.com/bins/juqqg`;
 
 const req = (url: string, method = 'GET', body?: any) => new Request(url, {
   method,
